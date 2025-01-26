024efdccdc5a70c2a87107fcb4a6008ced82690a
“richardcrn3rd” <richardcrn3rd@gmail.com>
2025-01-26 17:25:30 -0500

    Fixed SearchBar compile  error

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,16 +1,14 @@
-// SeachBar.js
 import { useState } from 'react'
 
-function SeachBar(props) {
+function SearchBar(props){
     let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-            <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
-            <input type="submit" />
-            
-        </form>
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+                <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
+                <input type="submit" />
+            </form>
     )
 }
 
-export default SeachBar
+export default SearchBar
