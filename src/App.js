101f47171e09ab7c17fc6ff182b1538618d5d2e2
“richardcrn3rd” <richardcrn3rd@gmail.com>
2025-01-26 17:17:43 -0500

    Update components gallery

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import {useEffect, useState } from 'react'
+import './App.css';
+import { useEffect, useState } from 'react'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
 
 function App() {
-	let [search, setSearch] = useState('')
-	let [message, setMessage] = useState('Search for Music!')
-	let [data, setData] = useState([])
+  let [searchTerm, setSearchTerm] = useState('')
+  let [data, setData] = useState([])
+  let [message, setMessage] = useState('Search for Music!')
 
-	const API_URL = 'https://itunes.apple.com/search?term='
+  useEffect(() => {
+    if (searchTerm) {
+      document.title=`${searchTerm} Music`
+      const fetchData = async () => {
+        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
+        const resData = await response.json()
+        if(resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('Not Found')
+        }
+      }
+      fetchData()
+  }
+  }, [searchTerm])
 
-	useEffect(() => {
-		if(search) {
-			const fetchData = async () => {
-				document.title = `${search} Music`
-				const response = await fetch(API_URL + search)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					setData(resData.results)
-				} else {
-					setMessage('Not Found')
-				}
-			}
-			fetchData()
-		}
-	}, [search])
-	
-	const handleSearch = (e, term) => {
-		e.preventDefault()
-		setSearch(term)
-	}
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearchTerm(term)
+  }
 
-	return (
-		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
-		</div>
-  	);
+  return (
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
+      {message}
+      <Gallery data={data} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -6,13 +6,9 @@ function SeachBar(props) {
 
     return (
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here" onChange={
-                (e) => setSearchTerm(e.target.value)
-            }/>
-
+            <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
             <input type="submit" />
-
+            
         </form>
     )
 }
